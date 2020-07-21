f62f093229c4f930a0b4233ecb31572a4301c52d
MariaRuano <maria.ruano@meaningfuldata.eu>
2020-07-21 11:59:09 +0200

    feat: create django css

--- polls/models.py
+++ polls/models.py
@@ -22,3 +22,12 @@ class Choice(models.Model):
 def was_published_recently(self):
     now = timezone.now()
     return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+class Question(models.Model):
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
