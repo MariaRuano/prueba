7bd927fdc3bd6f70dabd83049edf522c3f442d9a
MariaRuano <maria.ruano@meaningfuldata.eu>
2020-07-22 14:23:47 +0200

    feat: create django css

--- polls/tests.py
+++ polls/tests.py
@@ -7,9 +7,9 @@ from django.utils import timezone
 from .models import Question
 from django.urls import reverse
 
-    def create_question(question_text, days):
-        time = timezone.now() + datetime.timedelta(days=days)
-        return Question.objects.create(question_text=question_text, pub_date=time)
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
 
 class QuestionModelTests(TestCase):
 
